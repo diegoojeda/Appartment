7a94b3a61e0a0bfe05b6c81d31d9b09f2ffb74d4
Diego Ojeda García <diegoojedagarcia@gmail.com>
2015-07-01 17:00:53 +0200

    commit formateo pc

--- app/src/main/java/com/uma/tfg/appartment/broadcastReceivers/InstallReferrerReceiver.java
+++ app/src/main/java/com/uma/tfg/appartment/broadcastReceivers/InstallReferrerReceiver.java
@@ -7,17 +7,47 @@ import android.content.Intent;
 import com.uma.tfg.appartment.AppartmentSharedPreferences;
 import com.uma.tfg.appartment.util.Logger;
 
+import java.net.URLDecoder;
+
 //Test:
 //am broadcast -a com.android.vending.INSTALL_REFERRER -n com.uma.tfg.appartment/.broadcastReceivers.InstallReferrerReceiver --es "referrer" "prueba"
 
 public class InstallReferrerReceiver extends BroadcastReceiver{
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.hasExtra("referrer")) {
-            String referrer = intent.getStringExtra("referrer");
-            Logger.d("Recibido referrer " + referrer);
-            AppartmentSharedPreferences.setInstallReferrer(referrer);
+    //--------------------------------------------------------------------------
+    public InstallReferrerReceiver()
+    {
+        Logger.d("ReferrerReceiver.ReferrerReceiver()");
+    }
+
+    //--------------------------------------------------------------------------
+    @Override public void onReceive(Context context, Intent intent)
+    {
+        Logger.d("Recibido install intent");
+
+        try
+        {
+            // Make sure this is the intent we expect - it always should be.
+            if ((null != intent) && (intent.getAction().equals("com.android.vending.INSTALL_REFERRER")))
+            {
+                // This intent should have a referrer string attached to it.
+                String rawReferrer = intent.getStringExtra("referrer");
+                if (null != rawReferrer)
+                {
+                    // The string is usually URL Encoded, so we need to decode it.
+                    String referrer = URLDecoder.decode(rawReferrer, "UTF-8");
+
+                    // Log the referrer string.
+                    Logger.d("Recibido referrer: " + rawReferrer + " y " + referrer);
+
+                    // Persist the referrer string.
+                    AppartmentSharedPreferences.setInstallReferrer(referrer);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Logger.e(e.toString());
         }
     }
 }
